8c6ac0adb58f00f82cea2d2994b8f6aa1a4d2326
Rathsophea Sokomar <1020074@student.sae.edu.au>
2020-10-01 15:13:47 +1000

    Test
    Add !!! after hello world

--- Repositiory Test/Assets/TEST.cs
+++ Repositiory Test/Assets/TEST.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,7 +15,7 @@ public class TEST : MonoBehaviour
     {
         if (Input.GetKeyDown("s"))
         {
-            print("Hello World");
+            print("Hello World!!!");
         }
     }
 }
