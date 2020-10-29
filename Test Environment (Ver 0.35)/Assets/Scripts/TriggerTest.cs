2c2bdac4ac560c9281ae46b498ac25c1fbb49f3c
John <1018518@student.sae.edu.au>
2020-10-29 16:36:51 +1100

    Button implementation

---
+++ Test Environment (Ver 0.35)/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+{
+
+    public GameObject button;
+    public bool buttonActive;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (button != null)
+        {
+            button.SetActive(true);
+            buttonActive = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (button != null)
+        {
+            button.SetActive(false);
+            buttonActive = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonActive)
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                print("YIKES!");
+            }
+        }
+    }
+    
+
+}
