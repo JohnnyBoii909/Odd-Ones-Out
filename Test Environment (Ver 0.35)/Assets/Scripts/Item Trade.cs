a9e5586ef12a7cc144f0a4b8a7b5bcf616f68bc5
John <1018518@student.sae.edu.au>
2020-11-13 17:04:14 +1100

    (WIP) Item Trade system and Interract feature

---
+++ Test Environment (Ver 0.35)/Assets/Scripts/Item Trade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class ItemTrade : MonoBehaviour
+{
+    public List<Items> itemProgress;
+
+    public int maxItems = 5;
+
+    public bool haveItem(Items Item)
+    {
+        itemProgress.Add(Item);
+        return true;
+    }
+
+    private void checkProgress()
+    {
+        if (itemProgress.Count >= maxItems)
+        {
+            
+        }
+    }
+
+    
+
+    private void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}*/
