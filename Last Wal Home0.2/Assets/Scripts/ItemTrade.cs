1654d51cf60a147eca573ab16a0cd19983858387
Rathsophea Sokomar <1020074@student.sae.edu.au>
2020-11-19 15:57:18 +1100

    Repository Rebuilt

---
+++ Last Wal Home0.2/Assets/Scripts/ItemTrade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTrade : MonoBehaviour
+{
+    public List<Items> itemProgress;
+
+    public int maxItems = 5;
+
+    public bool haveItem(Items Item)
+    {
+        itemProgress.Add(Item);
+        print(itemProgress.Count);
+        return true;
+    }
+
+    private void checkProgress()
+    {
+        if (itemProgress.Count >= maxItems)
+        {
+            //Victory State
+        }
+    }
+
+    
+
+    private void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
